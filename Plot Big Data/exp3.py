dc4fe18dea2a62b5ce36d0ad7a8f0b40ea021844
Regis <florent_lin@ensae.fr>
2024-01-16 13:04:39 +0100

    new data

--- Plot Big Data/exp3.py
+++ Plot Big Data/exp3.py
@@ -19,7 +19,7 @@ def EXP_3(t_event, w, eta, arms, N):
     return (arms[A_t],w)
 
 
-def policy_evaluator_EXP3(dataframe, eta):
+def policy_evaluator_EXP3(dataframe, eta, loops):
     print('exp3 begins')
     # We get the list of the arms (the movies)
     arms = dataframe['movie_id'].unique().tolist()
@@ -27,17 +27,41 @@ def policy_evaluator_EXP3(dataframe, eta):
     w = np.ones(N, dtype = float)
     # We stock the payoffs in a list
     payoffs = []
-    decile = len(dataframe)/10
-    pourcent = 10
-    for t in range(len(dataframe)):
-        if t > decile:
-            print(f'progression : {pourcent} %')
-            decile += len(dataframe)/10
-            pourcent += 10
-        # We check our t-th row
-        t_event = dataframe[t : t + 1]
-        a_t , w = EXP_3(t_event , w, eta, arms, N)
-        # If the movie recommended matches the movie of our dataframe, we update our history and our payoffs
-        if a_t == t_event['movie_id'].iloc[0] :
-            payoffs.append(t_event['binary_rating'].iloc[0])
+    
+    for loop in range(loops):
+        
+        if loop == 0:
+            # Start with filtered data first
+            data = dataframe.copy()
+            # Initiate unused_data list
+            unused_data = []
+        
+        else:
+            # Recycle unused data
+            data = pd.DataFrame(unused_data, columns = dataframe.columns)
+            # Initiate unused_data list
+            unused_data = []
+            
+        print('Epoch ',loop )
+        # We keep track of our progression
+        decile = len(data)/10
+        pourcent = 10
+            
+        for t in range(len(data)):
+            
+            if t > decile:
+                print(f'progression : {pourcent} %')
+                decile += len(data)/10
+                pourcent += 10
+            
+            # We check our t-th row
+            t_event = data[t : t+1]
+            a_t , w = EXP_3(t_event , w, eta, arms, N)
+            # If the movie recommended matches the movie of our dataframe, we update our history and our payoffs
+            if a_t == t_event['movie_id'].iloc[0] :
+                payoffs.append(t_event['binary_rating'].iloc[0])
+            else:
+                # Recycle data
+                unused_data.append(data.iloc[t].tolist())
+                
     return payoffs
